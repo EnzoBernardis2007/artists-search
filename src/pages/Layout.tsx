99d47248d90c2b2ede4559725fbde70ca3270864
EnzoBernardis2007 <enzobernardis2007@gmail.com>
2025-03-18 17:59:12 -0300

    feat(credentials): getting credentials

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,6 +1,10 @@
+import { useEffect, useState } from "react"
 import { FaSearch } from "react-icons/fa"
 
 export default function Layout() {
+    const [accessToken, setAccessToken] = useState()
+    const clientId: string = "73e8a21edfc942fb85506fb935a1f252"
+    const clientSecret: string = "bbc2dc14882a4580bea1aa54bbdfdb7d"
     const suggestionsData: string[] = [
         "Maçã",
         "Banana",
@@ -14,8 +18,33 @@ export default function Layout() {
         "Manga"
     ]
 
+    useEffect(() => {
+        const getAccessToken = async () => {
+            const response = await fetch("https://accounts.spotify.com/api/token", {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/x-www-form-urlencoded",
+                    "Authorization": "Basic " + btoa(`${clientId}:${clientSecret}`)
+                },
+                body: new URLSearchParams({
+                    grant_type: "client_credentials"
+                })
+            })
+
+            if(response.ok) {
+                const data = await response.json()
+                setAccessToken(data.access_token)
+                console.log(data)
+            } else {
+                console.log("Unable to get access token")
+            }
+        }
+
+        getAccessToken()
+    }, [])
+
     return (
-        <div className="h-screen">
+        <div className="h-screen flex">
             {/* Profile */}
             <aside className="bg-neutral-950 h-screen w-1/3" >
                 <div className="w-full px-10 py-7 gap-3 flex">
@@ -31,6 +60,9 @@ export default function Layout() {
                     <button className="bg-green-500 h-9 w-11 rounded-full flex justify-center items-center hover:cursor-pointer"><FaSearch /></button>
                 </div>
             </aside>
+            <main className="bg-neutral-800 h-screen w-2/3">
+                <p>oi</p>
+            </main>
         </div>
     )
 }
